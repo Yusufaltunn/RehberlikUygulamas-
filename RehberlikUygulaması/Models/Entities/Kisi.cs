99f3adc1dbe8e7d1647d5197d64c3fc672267a18
Yusufaltunn <yusuf99altun@hotmail.com>
2022-06-05 22:11:01 +0300

    veritabanı işlemleri

---
+++ RehberlikUygulaması/Models/Entities/Kisi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcPhoneNumber.Models.entities
+{
+    public class kisi
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string CepTelefon { get; set; }
+        public int SehirId { get; set; }
+        public Sehir Sehir { get; set; }
+    }
+}
